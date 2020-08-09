39fbbad4b7eee2fec47eb8838166a76658189618
Brandon Arbini <b@arbini.dev>
2020-08-08 23:17:33 -0700

    Update content and add current mood.

--- components/brand-logos/index.js
+++ components/brand-logos/index.js
@@ -1,15 +1,17 @@
 import Buenas from "./buenas";
+import CurrentMood from "./current-mood";
 import FLDWRK from "./fldwrk";
 import FLDWRKStacked from "./fldwrk-stacked";
+import Formation from "./formation";
 import LetsBeKind from "./lets-be-kind";
 import Yellow from "./yellow";
-import Formation from "./formation";
 
 export default {
   Buenas,
+  CurrentMood,
   FLDWRK,
   FLDWRKStacked,
   LetsBeKind,
   Yellow,
-  Formation
+  Formation,
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,16 @@
-import React from "react";
-import Layout from "../components/layout";
-import brandLogos from "../components/brand-logos";
-import { SocialIcon } from "react-social-icons";
 import "./index.scss";
 
+import Layout from "../components/layout";
+import React from "react";
+import { SocialIcon } from "react-social-icons";
+import brandLogos from "../components/brand-logos";
+
 const {
+  CurrentMood: CurrentMoodLogo,
   FLDWRK: FLDWRKLogo,
   Yellow: YellowLogo,
   Buenas: BuenasLogo,
-  Formation: FormationLogo
+  Formation: FormationLogo,
   // LetsBeKind: LetsBeKindLogo,
   // Standard: StandardLogo
 } = brandLogos;
@@ -16,21 +18,19 @@ const {
 const Home = () => (
   <Layout className="home">
     <section className="intro">
-      <p>
+      {/* <p>
         At Common Good Network, we have a dream that one day, all cities will be
         full of redemptive communities creating symphonic justice. But there’s a
         problem--the challenges of this world are too overwhelming and complex
         for any single entity to solve alone. So we created a management firm to
-        lead redemptive communities in advancing the common good together. By
-        the end of 2020, our goal is for 6 Common Good Network brands to
-        accelerate 60 organizations and incubate 600 founders.
-      </p>
-      {/* <p>
+        lead redemptive communities in advancing the common good together.
+      </p> */}
+      <p>
         The challenges of the world are too overwhelming and difficult for any
         single entity to solve alone. We exist to lead our brands in advancing
         the common good, together. We manage a family of brands that each work
         to address the challenges of the world for the good of all people.
-      </p> */}
+      </p>
     </section>
     <section className="brands">
       <h2>Network of Brands</h2>
@@ -43,13 +43,13 @@ const Home = () => (
         <div>
           <p>
             FLDWRK has a dream that one day, all cities will be full of
-            organizations that are advancing the common good together. But
-            there’s a problem--working alone robs leaders of their humanity and
+            organizations that are advancing the common good together.
+            {/* But there’s a problem--working alone robs leaders of their humanity and
             impact. So we built a social good incubator that guides leaders and
             startups to collectively build organizations into forces for good in
             their cities. Currently, we’re working so that by the end of 2020,
             400 creators are building 44 organizations into forces for good in
-            10 cities.
+            10 cities. */}
           </p>
           <a href="https://fldwrk.io" className="btn btn-dark brandLink">
             fldwrk.io
@@ -66,11 +66,11 @@ const Home = () => (
           <p>
             Yellow has a dream that one day, all female leaders will know
             themselves to lead themselves to lead others into meaningful work.
-            But there’s a problem--boss culture requires disconnection and
+            {/* But there’s a problem--boss culture requires disconnection and
             inhibits change. So we created an empowering community that convenes
             and guides female leaders into meaningful work. By the end of 2020,
             our goal is for 2,500 Yellow members to be collectively working as
-            agents of good in their cities.
+            agents of good in their cities. */}
           </p>
           <a href="https://yellowco.co" className="btn btn-dark brandLink">
             yellowco.co
@@ -86,12 +86,13 @@ const Home = () => (
         <div>
           <p>
             Formation has a dream that one day all cities will be full of
-            organizations advancing the common good together. But there’s a
+            organizations advancing the common good together.
+            {/* But there’s a
             problem: Our culture of individualism along with positional
             isolation robs top leaders, their organizations, and cities of their
             humanity and impact. We exist for top leaders to build organizations
             into forces for good in their cities. By the end of 2020, our goal
-            is to have 3 certified guides leading 20 top leaders.
+            is to have 3 certified guides leading 20 top leaders. */}
           </p>
           <a href="https://formation.ceo" className="btn btn-dark brandLink">
             formation.ceo
@@ -107,61 +108,33 @@ const Home = () => (
         <div>
           <p>
             Buenas has a dream that one day, third places would host people with
-            honor, generosity, and presence. But there’s a problem--greed
+            honor, generosity, and presence.
+            {/* But there’s a problem--greed
             reduces people to transactions. So we’ve created a coffee shop
             community that fuels good people to do good work in the world.
             Currently, were working so that by the end of 2020, over 60,000
             drinks and meals are fueling good people, 200 of which are actively
             engaged in a more sustainable lifestyle as a result of their
-            membership in the community.
+            membership in the community. */}
           </p>
           <a href="https://buenas.coffee" className="btn btn-dark brandLink">
             buenas.coffee
           </a>
         </div>
       </div>
-      {/* <div className="brand">
+      <div className="brand">
         <div className="brandLogo">
-          <a href="https://letsbekind.org">
-            <LetsBeKindLogo className="letsBeKindLogo" />
+          <a href="https://currentmood.is">
+            <CurrentMoodLogo className="currentMoodLogo" />
           </a>
         </div>
         <div>
-          <p>
-            Let’s Be Kind Day is a special event on school campuses that reminds
-            people that they are cared for, loved and valued. On the school
-            campus those three words are hard to know and feel but KINDNESS you
-            can see. The hope is that kindness will bring a group of students
-            together to become the most incredible community that their school
-            has ever seen.
-          </p>
-          <a href="https://letsbekind.org" className="btn btn-dark brandLink">
-            letsbekind.org
+          <p>Know how you feel. Act with Intention</p>
+          <a href="https://currentmood.is" className="btn btn-dark brandLink">
+            currentmood.is
           </a>
         </div>
       </div>
-      <div className="brand">
-        <div className="brandLogo">
-          <a href="https://wearestandard.org">
-            {(StandardLogo && <StandardLogo className="standardLogo" />) ||
-              "???"}
-          </a>
-        </div>
-        <div>
-          <p>
-            Est qui quidem aut ratione quo sed vel officiis voluptas. Ducimus
-            expedita reiciendis magnam sint sapiente. Repellendus atque minima
-            et laboriosam provident recusandae harum impedit et. Suscipit nam
-            officia nam minima. Vitae facere ullam.
-          </p>
-          <a
-            href="https://wearestandard.org"
-            className="btn btn-dark brandLink"
-          >
-            wearestandard.org
-          </a>
-        </div>
-      </div> */}
     </section>
     <section className="team">
       <h2>Leadership Team</h2>
